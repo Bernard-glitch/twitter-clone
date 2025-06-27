ba40ab8c3374135918864a8f66c0863a98226e1d
Bernard <twistor75@gmail.com>
2025-06-27 14:02:56 +0800

    Commit3

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -5,7 +5,7 @@ import {
     signInWithEmailAndPassword,
     signInWithPopup,
 } from "firebase/auth";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Button, Col, Form, Image, Modal, Row } from "react-bootstrap";
 import { AuthContext } from "../components/AuthProvider";
 import { useNavigate } from "react-router-dom";
@@ -23,7 +23,10 @@ export default function AuthPage() {
     const { currentUser } = useContext(AuthContext);
     const provider = new GoogleAuthProvider();
 
-    if (currentUser) navigate("/profile");
+    useEffect(() => {
+        if (currentUser) { navigate("/profile"); }
+    }, [currentUser, navigate]);
+
 
     const handleSignUp = async (e) => {
         e.preventDefault();
